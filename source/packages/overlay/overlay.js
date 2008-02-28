b78f843d0a6286a00a3e9302b40949a9da5d31e2
James Coglan <jcoglan@googlemail.com>
2008-02-28 17:48:10 +0000

    Restructuring Overlay using JS.State.

--- source/packages/overlay/overlay.js
+++ source/packages/overlay/overlay.js
@@ -1,10 +1,4 @@
-var INVISIBLE   = 1,
-    SHOWING     = 2,
-    VISIBLE     = 3,
-    HIDING      = 4,
-    CLOSED      = 5,
-    
-    stub = function() { return this; };
+var stub = function() { return this; };
 
 /**
  * <p>The <tt>Overlay</tt> class is designed to model the most basic container for overlaying
@@ -17,6 +11,8 @@ var INVISIBLE   = 1,
  * @class Overlay
  */
 Ojay.Overlay = JS.Class({
+    include: [JS.State, JS.Observable],
+    
     extend: {
         BASE_LAYER:         10000,
         MINIMUM_OFFSET:     20,
@@ -64,7 +60,7 @@ Ojay.Overlay = JS.Class({
         this._elements = {};
         options = this._options = options || {};
         Ojay(document.body).insert(this.getHTML().node, 'top');
-        this._setState(INVISIBLE);
+        this.setState('INVISIBLE');
         this.setSize(options.width, options.height);
         this.setPosition(options.left, options.top);
         this.setContent(options.content);
@@ -96,12 +92,12 @@ Ojay.Overlay = JS.Class({
      * @returns {Overlay}
      */
     setPosition: function(left, top) {
-        if (this._inState(CLOSED)) return this;
+        if (this.inState('CLOSED')) return this;
         var defaults = this.klass.DEFAULT_POSITION;
         left = this._addUnits(left === undefined ? defaults.left : left);
         top = this._addUnits(top === undefined ? defaults.top : top);
         this._position = {left: left, top: top};
-        if (this._inState(VISIBLE))
+        if (this.inState('VISIBLE'))
             this._elements._container.setStyle(this._position);
         return this;
     },
@@ -117,31 +113,17 @@ Ojay.Overlay = JS.Class({
     },
     
     /**
-     * @returns {Overlay}
-     */
-    center: function() {
-        if (this._inState(CLOSED, INVISIBLE)) return this;
-        var region = this.getRegion();
-        var viewport = Ojay.getViewportSize(), scrolls = Ojay.getScrollOffsets(),
-            left = scrolls.left + viewport.width/2 - region.getWidth()/2,
-            top = scrolls.top + viewport.height/2.2 - region.getHeight()/2;
-        if (left < this.klass.MINIMUM_OFFSET) left = this.klass.MINIMUM_OFFSET;
-        if (top < this.klass.MINIMUM_OFFSET) top = this.klass.MINIMUM_OFFSET;
-        return this.setPosition(left, top);
-    },
-    
-    /**
      * @param {Number|String} width
      * @param {Number|String} height
      * @returns {Overlay}
      */
     setSize: function(width, height) {
-        if (this._inState(CLOSED)) return this;
+        if (this.inState('CLOSED')) return this;
         var defaults = this.klass.DEFAULT_SIZE;
         width = this._addUnits(width === undefined ? defaults.width : width);
         height = this._addUnits(height === undefined ? defaults.height : height);
         this._dimensions = {width: width, height: height};
-        if (this._inState(VISIBLE))
+        if (this.inState('VISIBLE'))
             this._elements._container.setStyle(this._dimensions);
         return this;
     },
@@ -160,18 +142,9 @@ Ojay.Overlay = JS.Class({
      * @returns {Region}
      */
     getRegion: function() {
-        return !this._inState(INVISIBLE, CLOSED)
+        return !this.inState('INVISIBLE', 'CLOSED')
                 ? this._elements._container.getRegion()
                 : undefined;
-    },
-    
-    /**
-     * @returns {Overlay}
-     */
-    fitToContent: function() {
-        if (!this._inState(VISIBLE)) return this;
-        var region = this._elements._content.getRegion();
-        return this.setSize(region.getWidth(), region.getHeight());
     },
     
     /**
@@ -183,7 +156,7 @@ Ojay.Overlay = JS.Class({
                 ? this.klass.DEFAULT_OPACITY
                 : Number(opacity);
         if (this._opacity > 1) this._opacity /= 100;
-        if (this._inState(VISIBLE))
+        if (this.inState('VISIBLE'))
             this._elements._container.setStyle({opacity: this._opacity});
         return this;
     },
@@ -216,7 +189,7 @@ Ojay.Overlay = JS.Class({
      * @returns {Overlay}
      */
     setLayer: function(index) {
-        if (this._inState(CLOSED)) return this;
+        if (this.inState('CLOSED')) return this;
         this._layer = (index === undefined) ? this.klass.BASE_LAYER : Number(index);
         this._elements._container.setStyle({zIndex: this._layer});
         return this;
@@ -234,7 +207,7 @@ Ojay.Overlay = JS.Class({
      * @returns {Overlay}
      */
     setContent: function(content) {
-        if (this._inState(CLOSED)) return this;
+        if (this.inState('CLOSED')) return this;
         this._elements._content.setContent(content || '');
         return this;
     },
@@ -245,61 +218,64 @@ Ojay.Overlay = JS.Class({
      * @returns {Overlay}
      */
     insert: function(content, position) {
-        if (this._inState(CLOSED)) return this;
+        if (this.inState('CLOSED')) return this;
         this._elements._content.insert(content, position);
         return this;
     },
     
-    /**
-     * @param {String} transition
-     * @returns {Overlay|MethodChain}
-     */
-    show: function(transition) {
-        if (!this._inState(INVISIBLE)) return this;
-        this._setState(SHOWING);
-        transition = this.klass.Transitions.get(transition || 'none');
-        var chain = new JS.MethodChain()._(this)._setState(VISIBLE);
-        return transition.show(this, chain);
-    },
-    
-    /**
-     * @param {String} transition
-     * @returns {Overlay|MethodChain}
-     */
-    hide: function(transition) {
-        if (!this._inState(VISIBLE)) return this;
-        this._setState(HIDING);
-        transition = this.klass.Transitions.get(transition || 'none');
-        var chain = new JS.MethodChain()._(this)._setState(INVISIBLE);
-        return transition.hide(this, chain);
-    },
-    
-    /**
-     * @param {String} transition
-     * @returns {Overlay}
-     */
-    close: function(transition) {
-        if (this._inState(CLOSED)) return this;
-        if (this._inState(VISIBLE)) return this.hide(transition).close();
-        this._elements._container.remove();
-        this._setState(CLOSED);
-        return this;
+    states: {
+        
+        INVISIBLE:  {
+            show: function(transition) {
+                this.setState('SHOWING');
+                transition = this.klass.Transitions.get(transition || 'none');
+                var chain = new JS.MethodChain()._(this).setState('VISIBLE');
+                return transition.show(this, chain);
+            },
+            
+            close: function() {
+                this._elements._container.remove();
+                this.setState('CLOSED');
+                return this;
+            }
+        },
+        
+        SHOWING:    {},
+        
+        VISIBLE:    {
+            center: function() {
+                var region = this.getRegion();
+                var viewport = Ojay.getViewportSize(), scrolls = Ojay.getScrollOffsets(),
+                    left = scrolls.left + viewport.width/2 - region.getWidth()/2,
+                    top = scrolls.top + viewport.height/2.2 - region.getHeight()/2;
+                if (left < this.klass.MINIMUM_OFFSET) left = this.klass.MINIMUM_OFFSET;
+                if (top < this.klass.MINIMUM_OFFSET) top = this.klass.MINIMUM_OFFSET;
+                return this.setPosition(left, top);
+            },
+            
+            fitToContent: function() {
+                var region = this._elements._content.getRegion();
+                return this.setSize(region.getWidth(), region.getHeight());
+            },
+            
+            hide: function(transition) {
+                this.setState('HIDING');
+                transition = this.klass.Transitions.get(transition || 'none');
+                var chain = new JS.MethodChain()._(this).setState('INVISIBLE');
+                return transition.hide(this, chain);
+            },
+            
+            close: function(transition) {
+                return this.hide(transition)._(this).close();
+            }
+        },
+        
+        HIDING:     {},
+        
+        CLOSED:     {}
     },
     
     _addUnits: function(x) {
         return String(x).replace(/^(-?\d+(?:\.\d+)?)$/g, '$1px');
-    },
-    
-    _setState: function(state) {
-        this._state = state;
-        return this;
-    },
-    
-    _inState: function() {
-        var n = arguments.length;
-        while (n--) {
-            if (this._state == arguments[n]) return true;
-        }
-        return false;
     }
 });
