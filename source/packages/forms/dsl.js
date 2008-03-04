0386d2fca7abeecc283a52ed00fd3a23d33880a0
James Coglan <jcoglan@googlemail.com>
2008-03-04 17:59:25 +0000

    Adding some documentation to Forms DSL objects.

--- source/packages/forms/dsl.js
+++ source/packages/forms/dsl.js
@@ -19,21 +19,51 @@
  *     }});
  */
 
+// Store to hold sets of form rules, entry per page form.
 var forms = {};
 
+/**
+ * <p>Returns the <tt>FormDescription</tt> for the given <tt>id</tt>. A new description is
+ * created if one does not already exist for the <tt>id</tt>.</p>
+ * @param {String} id The HTML id of the form
+ * @private
+ */
 var getForm = function(id) {
     return forms[id] || (forms[id] = new FormDescription(id));
 };
 
+/**
+ * <p>This is the main DSL object for <tt>Ojay.Forms</tt>. It contains methods that should
+ * act as top-level functions in the DSL. Do not put a method in here unless it needs to be
+ * a top-level function.</p>
+ * @private
+ */
 var DSL = {
+    /**
+     * <p>Returns a DSL object for describing the form with the given <tt>id</tt>.</p>
+     * @param {String} id The id of the form you want to describe
+     * @returns {FormDSL}
+     */
     form: function(id) {
         return getForm(id).dsl || null;
     },
     
+    /**
+     * <p>Returns a DSL object for describing the form with the given <tt>id</tt>.</p>
+     * @param {String} id The id of the form you want to describe
+     * @returns {WhenDSL}
+     */
     when: function(id) {
         return getForm(id).when || null;
     },
     
+    /**
+     * <p>Returns a helper function for use with <tt>when().isValidated()</tt>. The returned
+     * function will display the forms elements as a bulleted list inside the element you
+     * supply, in a <tt>DIV</tt> with the class name <tt>error-explanation</tt>.</p>
+     * @param {String|HTMLElement|DomCollection} element
+     * @returns {Function}
+     */
     displayErrorsIn: function(element) {
         element = Ojay(element);
         return function(errors) {
@@ -50,11 +80,28 @@ var DSL = {
     EMAIL_FORMAT: Ojay.Forms.EMAIL_FORMAT
 };
 
+/**
+ * <p>The <tt>FormDSL</tt> class creates DSL objects used to describe forms. Every
+ * <tt>FormDescription</tt> instance has one of these objects that provides DSL-level
+ * access to the description.</p>
+ * @constructor
+ * @class FormDSL
+ * @private
+ */
 var FormDSL = JS.Class({
+    /**
+     * @param {FormDescription} form
+     */
     initialize: function(form) {
         this.form = form;
     },
     
+    /**
+     * <p>Returns a <tt>RequirementDSL</tt> object used to specify the requirement.</p>
+     * @param {String} name
+     * @param {String} displayed
+     * @returns {RequirementDSL}
+     */
     requires: function(name, displayed) {
         var requirement = this.form.getRequirement(name);
         requirement.name = displayed;
@@ -62,11 +109,28 @@ var FormDSL = JS.Class({
     }
 });
 
+/**
+ * <p>The <tt>RequirementDSL</tt> class creates DSL objects used to describe form requirements.
+ * All <tt>FormRequirement</tt> objects have one of these objects associated with them.</p>
+ * @constructor
+ * @class RequirementDSL
+ * @private
+ */
 var RequirementDSL = JS.Class({
+    /**
+     * @param {FormRequirement} requirement
+     */
     initialize: function(requirement) {
         this.requirement = requirement;
     },
     
+    /**
+     * <p>Specifies that the required field must have a certain length in order to be considered
+     * valid. Valid inputs are a number (to specifiy an exact length), or an object with
+     * <tt>minimum</tt> and <tt>maximum</tt> fields.</p>
+     * @param {Number|Object} options
+     * @returns {RequirementDSL}
+     */
     toHaveLength: function(options) {
         var min = options.minimum, max = options.maximum;
         this.requirement.add(function(value) {
@@ -81,6 +145,12 @@ var RequirementDSL = JS.Class({
         return this;
     },
     
+    /**
+     * <p>Specifies that the required field must have a certain value in order to be considered
+     * valid. Input should be an object with <tt>minimum</tt> and <tt>maximum</tt> fields.</p>
+     * @param {Object} options
+     * @returns {RequirementDSL}
+     */
     toHaveValue: function(options) {
         var min = options.minimum, max = options.maximum;
         this.requirement.add(function(value) {
@@ -95,6 +165,12 @@ var RequirementDSL = JS.Class({
         return this;
     },
     
+    /**
+     * <p>Specifies that the required field must match a given regex in order to be considered
+     * valid.</p>
+     * @param {Regexp} format
+     * @returns {RequirementDSL}
+     */
     toMatch: function(format) {
         this.requirement.add(function(value) {
             return format.test(value) || 'is not valid';
@@ -102,6 +178,10 @@ var RequirementDSL = JS.Class({
         return this;
     },
     
+    /**
+     * <p>Specifies that the required field must be a number in order to be considered valid.</p>
+     * @returns {RequirementDSL}
+     */
     toBeNumeric: function() {
         this.requirement.add(function(value) {
             return Ojay.Forms.isNumeric(value) || 'is not a number';
@@ -109,16 +189,37 @@ var RequirementDSL = JS.Class({
         return this;
     },
     
+    /**
+     * <p>Provided to allow you to return to the <tt>FormDSL</tt>you want using after entering
+     * into using a <tt>RequirementDSL</tt> object.</p>
+     * @param {String} name
+     * @returns {RequirementDSL}
+     */
     requires: function(name) {
         return this.requirement.form.dsl.requires(name);
     }
 });
 
+/**
+ * <p>The <tt>WhenDSL</tt> class creates DSL objects used to describe form requirements. All
+ * <tt>FormRequirement</tt> objects have one of these objects associated with them. The WhenDSL
+ * is used specifically to describe events linked to forms.</p>
+ * @constructor
+ * @class RequirementDSL
+ * @private
+ */
 var WhenDSL = JS.Class({
+    /**
+     * @param {FormDescription} form
+     */
     initialize: function(form) {
         this.form = form;
     },
     
+    /**
+     * <p>Allows a hook to be registered to say what should be done with the list of error
+     * messages when a particular form is validated.</p>
+     */
     isValidated: function(block, context) {
         this.form.subscribe(function(form) {
             block.call(context || null, form.errors);
