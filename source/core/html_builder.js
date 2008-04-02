3edb7727521c7c436196bd8ea745af14ed2fcfc5
James Coglan <jcoglan@googlemail.com>
2008-04-02 10:11:17 +0000

    Making HtmlBuilder a little smaller. Latest PackR has knocked a bit off the build size as well.

--- source/core/html_builder.js
+++ source/core/html_builder.js
@@ -123,18 +123,15 @@ Ojay.HtmlBuilder = JS.Class(/* @scope Ojay.HtmlBuilder.prototype */{
          * List of all HTML 4.01 tag names, culled from the <a
          * href="http://www.w3.org/TR/REC-html40/index/elements.html">W3C spec</a>.
          */
-        TAG_NAMES: [
-            "a", "abbr", "acronym", "address", "applet", "area", "b", "base", "basefont",
-            "bdo", "big", "blockquote", "body", "br", "button", "caption", "center", "cite",
-            "code", "col", "colgroup", "dd", "del", "dfn", "dir", "div", "dl", "dt", "em", "embed",
-            "fieldset", "font", "form", "frame", "frameset", "h1", "h2", "h3", "h4", "h5", "h6",
-            "head", "hr", "html", "i", "iframe", "img", "input", "ins", "isindex", "kbd",
-            "label", "legend", "li", "link", "map", "menu", "meta", "noframes", "noscript",
-            "object", "ol", "optgroup", "option", "p", "param", "pre", "q", "s", "samp",
-            "script", "select", "small", "span", "strike", "strong", "style", "sub", "sup",
-            "table", "tbody", "td", "textarea", "tfoot", "th", "thead", "title", "tr", "tt",
-            "u", "ul", "var"
-        ]
+        TAG_NAMES: (
+            "a abbr acronym address applet area b base basefont bdo big blockquote body " +
+            "br button caption center cite code col colgroup dd del dfn dir div dl dt em " +
+            "embed fieldset font form frame frameset h1 h2 h3 h4 h5 h6 head hr html i " +
+            "iframe img input ins isindex kbd label legend li link map menu meta noframes " +
+            "noscript object ol optgroup option p param pre q s samp script select small " +
+            "span strike strong style sub sup table tbody td textarea tfoot th thead title " +
+            "tr tt u ul var"
+        ).split(/\s+/)
     }
 });
 
