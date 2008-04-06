c3d7378a7d4ec828018a5b350186e20c1886c21b
James Coglan <jcoglan@googlemail.com>
2008-04-06 21:28:08 +0000

    Adding some basic keyboard rule tests.

---
+++ site/site/javascripts/test/keyboard.js
@@ -0,0 +1,20 @@
+Ojay('.buzzer').setStyle({opacity: 0});
+
+var buzz = function(selector) {
+    Ojay(selector)
+        .setStyle({opacity: 1})
+        .wait(0.3)
+        .animate({opacity: {from: 1, to: 0}}, 0.8);
+}.curry(3);
+
+var shiftZ = Ojay.Keyboard.listen(document, 'SHIFT Z', buzz('#block1 .buzzer'));
+shiftZ.preventDefault();
+Ojay('#block1 .disable').on('click', Ojay.stopEvent)._(shiftZ).disable();
+Ojay('#block1 .enable').on('click', Ojay.stopEvent)._(shiftZ).enable();
+
+var ctrlS = Ojay.Keyboard.listen(document, 'CONTROL S', buzz('#block2 .buzzer'));
+ctrlS.preventDefault();
+Ojay('#block2 .disable').on('click', Ojay.stopEvent)._(ctrlS).disable();
+Ojay('#block2 .enable').on('click', Ojay.stopEvent)._(ctrlS).enable();
+Ojay('#block2 .allow').on('click', Ojay.stopEvent)._(ctrlS).allowDefault();
+Ojay('#block2 .deny').on('click', Ojay.stopEvent)._(ctrlS).preventDefault();
