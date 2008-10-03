51835b88de67714c8521265fa415861d4ceab079
James Coglan <jcoglan@googlemail.com>
2008-10-03 16:29:24 +0000

    Bringing Accordion documentation into the public site. Need documentation for events, methods and some examples.

--- source/packages/accordion/accordion.js
+++ source/packages/accordion/accordion.js
@@ -7,18 +7,18 @@
  * <p>Creating an accordion is straightforward. Start with a list of elements, each of which
  * should contain an element that you want to collapse. For example:</p>
  *
- * <pre><code>    <div class="section">
- *         <h3>Section 1</h3>
- *         <p>Lorem ipsum...</p>
- *     </div>
- *     <div class="section">
- *         <h3>Section 2</h3>
- *         <p>Dolor sit amet...</p>
- *     </div>
- *     <div class="section">
- *         <h3>Section 3</h3>
- *         <p>Quaniam omnes...</p>
- *     </div></code></pre>
+ * <pre><code>    &lt;div class="section"&gt;
+ *         &lt;h3&gt;Section 1&lt;/h3&gt;
+ *         &lt;p&gt;Lorem ipsum...&lt;/p&gt;
+ *     &lt;/div&gt;
+ *     &lt;div class="section"&gt;
+ *         &lt;h3&gt;Section 2&lt;/h3&gt;
+ *         &lt;p&gt;Dolor sit amet...&lt;/p&gt;
+ *     &lt;/div&gt;
+ *     &lt;div class="section"&gt;
+ *         &lt;h3&gt;Section 3&lt;/h3&gt;
+ *         &lt;p&gt;Quaniam omnes...&lt;/p&gt;
+ *     &lt;/div&gt;</code></pre>
  *
  * <p>In this example, the paragraphs will collapse and the headings will be the menu tabs.
  * The following script sets up the menu:</p>
@@ -29,24 +29,24 @@
  * <p>This will make a few changes to the markup, which you'll need to be aware of in order
  * to apply CSS. After the script runs, the document will look like this:</p>
  *
- * <pre><code>    <div class="section accordion-section">
- *         <h3>Section 1</h3>
- *     </div>
- *     <div class="accordion-collapsible">
- *         <p>Lorem ipsum...</p>
- *     </div>
- *     <div class="section accordion-section">
- *         <h3>Section 2</h3>
- *     </div>
- *     <div class="accordion-collapsible">
- *         <p>Dolor sit amet...</p>
- *     </div>
- *     <div class="section accordion-section">
- *         <h3>Section 3</h3>
- *     </div>
- *     <div class="accordion-collapsible">
- *         <p>Quaniam omnes...</p>
- *     </div></code></pre>
+ * <pre><code>    &lt;div class="section accordion-section"&gt;
+ *         &lt;h3&gt;Section 1&lt;/h3&gt;
+ *     &lt;/div&gt;
+ *     &lt;div class="accordion-collapsible"&gt;
+ *         &lt;p&gt;Lorem ipsum...&lt;/p&gt;
+ *     &lt;/div&gt;
+ *     &lt;div class="section accordion-section"&gt;
+ *         &lt;h3&gt;Section 2&lt;/h3&gt;
+ *     &lt;/div&gt;
+ *     &lt;div class="accordion-collapsible"&gt;
+ *         &lt;p&gt;Dolor sit amet...&lt;/p&gt;
+ *     &lt;/div&gt;
+ *     &lt;div class="section accordion-section"&gt;
+ *         &lt;h3&gt;Section 3&lt;/h3&gt;
+ *     &lt;/div&gt;
+ *     &lt;div class="accordion-collapsible"&gt;
+ *         &lt;p&gt;Quaniam omnes...&lt;/p&gt;
+ *     &lt;/div&gt;</code></pre>
  *
  * <p>The original sections get an additional class to indicate that they're part of an
  * <tt>Accordion</tt> instance, and the collapsible elements get placed inside a new wrapper
