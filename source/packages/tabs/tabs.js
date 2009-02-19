22a3a9911d63faec1b2026f5f60443381e974f7a
Benedict Eastaugh <benedict@eastaugh.net>
2009-02-19 11:35:20 +0000

    Prefix private variables with underscores.

--- source/packages/tabs/tabs.js
+++ source/packages/tabs/tabs.js
@@ -13,27 +13,27 @@ Ojay.Tabs = new JS.Class(/** @scope Ojay.Tabs.prototype */{
     options = this._options = options || {};
     
     options.toggleSelector = options.toggleSelector || this.klass.TOGGLE_SELECTOR;
-    options.togglesClass = options.togglesClass || this.klass.TOGGLES_CLASS;
-    options.switchTime = options.switchTime || this.klass.SWITCH_TIME;
-    options.tabsPosition = options.tabsPosition || 'before';
+    options.togglesClass   = options.togglesClass   || this.klass.TOGGLES_CLASS;
+    options.switchTime     = options.switchTime     || this.klass.SWITCH_TIME;
+    options.tabsPosition   = options.tabsPosition   || 'before';
     
-    this.container = Ojay(tabs).parents().at(0);
+    this._container = Ojay(tabs).parents().at(0);
     
     this.addToggles(tabs);
     
-    this.tabs = Ojay(tabs).map(function(container) {
+    this._tabs = Ojay(tabs).map(function(container) {
       return new this.klass.Tab(this, Ojay(container));
     }.bind(this));
     
     if (options.width && options.height)
-      this.container.setStyle({height: options.height});
+      this._container.setStyle({height: options.height});
     
     this.toggle(0);
   },
   
   addToggles: function(tabs) {
     var self = this;
-    this.toggles = [];
+    this._toggles = [];
     
     var toggles = Ojay(Ojay.HTML.ul({className: this._options.togglesClass},
     function (H) {
@@ -43,7 +43,7 @@ Ojay.Tabs = new JS.Class(/** @scope Ojay.Tabs.prototype */{
         var toggle = Ojay(H.li(header.node.innerHTML)).addClass('toggle-' + i);
         if (i === 0) toggle.addClass('first');
         if (i === tabs.length - 1) toggle.addClass('last');
-        self.toggles.push(toggle);
+        self._toggles.push(toggle);
         toggle.on('click', function() { self.toggle(i); });
       });
     }));
@@ -57,21 +57,21 @@ Ojay.Tabs = new JS.Class(/** @scope Ojay.Tabs.prototype */{
   toggle: function(index, options) {
     options = options || {};
     
-    if (index >= this.tabs.length) index = 0;
-    if (this.currentTab == index || this.animating) return;
+    if (index >= this._tabs.length) index = 0;
+    if (this._currentTab == index || this._animating) return;
     
-    if (typeof this.currentTab == 'undefined') {
-      this.currentTab = index;
-      this.toggles[index].addClass('current');
-      this.tabs[index].show();
+    if (typeof this._currentTab == 'undefined') {
+      this._currentTab = index;
+      this._toggles[index].addClass('current');
+      this._tabs[index].show();
     } else {
-      this.animating = true;
-      this.toggles.forEach(function(toggle) { toggle.removeClass('current'); });
-      this.toggles[index].addClass('current');
-      this.tabs[this.currentTab].hide()._(function(self) {
-        self.currentTab = index;
-        self.tabs[index].show()._(function() {
-          self.animating = false;
+      this._animating = true;
+      this._toggles.forEach(function(toggle) { toggle.removeClass('current'); });
+      this._toggles[index].addClass('current');
+      this._tabs[this._currentTab].hide()._(function(self) {
+        self._currentTab = index;
+        self._tabs[index].show()._(function() {
+          self._animating = false;
           if (options.silent !== true) self.notifyObservers('tabchange', index);
         });
       }, this);
@@ -86,26 +86,26 @@ Ojay.Tabs = new JS.Class(/** @scope Ojay.Tabs.prototype */{
     Tab: new JS.Class({
       
       initialize: function(group, container) {
-        this.container = container, this.group = group;
+        this._container = container, this._group = group;
         
-        this.container.hide().setStyle({opacity: 0});
+        this._container.hide().setStyle({opacity: 0});
         
-        if (this.group._options.height)
-          this.container.setStyle({position: 'absolute', top: 0, left: 0});
+        if (this._group._options.height)
+          this._container.setStyle({position: 'absolute', top: 0, left: 0});
       },
       
       hide: function() {
         var chain = new JS.MethodChain;
-        this.container.animate({opacity: {to: 0}},
-          this.group._options.switchTime).hide()
+        this._container.animate({opacity: {to: 0}},
+          this._group._options.switchTime).hide()
         ._(function(self) { chain.fire(self); }, this)._(this);
         return chain;
       },
       
       show: function() {
         var chain = new JS.MethodChain;
-        this.container.show().animate({opacity: {to: 1}},
-          this.group._options.switchTime)
+        this._container.show().animate({opacity: {to: 1}},
+          this._group._options.switchTime)
         ._(function(self) { chain.fire(self); }, this)._(this);
         return chain;
       }
