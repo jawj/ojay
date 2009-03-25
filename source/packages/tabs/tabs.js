522835ef511bc057ae263d2e19b21f14079143ff
James Coglan <jcoglan@googlemail.com>
2009-03-25 16:13:37 +0000

    Adding JS.State to Tabs class as its state is no longer a simple binary choice and this prevents overlapping animations slightly more robustly. Also changing the name of some API methods and events to match Paginator API as these two widgets do broadly similar things and the polymorphism could be useful here. And, routing Tabs#setPage through the State#changeState method and a state handler function, so that calls create history entries.

--- source/packages/tabs/tabs.js
+++ source/packages/tabs/tabs.js
@@ -3,7 +3,7 @@
  * @class Tabs
  */
 Ojay.Tabs = new JS.Class(/** @scope Ojay.Tabs.prototype */{
-    include: Ojay.Observable,
+    include: [Ojay.Observable, JS.State],
       
     /**
      * @param {String} tabs
@@ -12,34 +12,13 @@ Ojay.Tabs = new JS.Class(/** @scope Ojay.Tabs.prototype */{
     initialize: function(tabs, options) {
         this._tabGroup = tabs;
         this._options  = options || {};
+        this.setState('CREATED');
     },
     
     /**
      * @returns {Tabs}
      */
-    setup: function() {
-        var options = this._options;
-        options.toggleSelector = options.toggleSelector || this.klass.TOGGLE_SELECTOR;
-        options.togglesClass   = options.togglesClass   || this.klass.TOGGLES_CLASS;
-        options.switchTime     = options.switchTime     || this.klass.SWITCH_TIME;
-        options.tabsPosition   = options.tabsPosition   || this.klass.INSERT_POSITION;
-        
-        this._tabGroup  = Ojay(this._tabGroup);
-        this._container = this._tabGroup.parents().at(0);
-        
-        this._addToggles();
-        
-        this._tabs = this._tabGroup.map(function(container) {
-            return new this.klass.Tab(this, container);
-        }, this);
-        
-        if (options.width && options.height)
-            this._container.setStyle({height: options.height});
-        
-        var state = this.getInitialState();
-        this.toggle(state.tab);
-        return this;
-    },
+    
     
     /**
      * @returns {Object}
@@ -53,67 +32,105 @@ Ojay.Tabs = new JS.Class(/** @scope Ojay.Tabs.prototype */{
      * @returns {Tabs}
      */
     changeState: function(state) {
-        if (state.tab !== undefined) this.toggle(state.tab);
+        if (state.tab !== undefined) this._handleSetPage(state.tab);
         return this;
     },
     
-    /**
-     * <p>Insert the toggle control group before or after the tabs' containing
-     * element.</p>
-     */
-    _addToggles: function() {
-        this._toggles = [];
-        var self = this;
-        var toggles = Ojay(Ojay.HTML.ul({className: this._options.togglesClass},
-        function (H) {
-            self._tabGroup.children(self._options.toggleSelector)
-            .forEach(function(header, i) {
-                header.hide();
-                var toggle = Ojay(H.li(header.node.innerHTML)).addClass('toggle-' + i);
-                if (i === 0) toggle.addClass('first');
-                if (i === self._tabGroup.length - 1) toggle.addClass('last');
-                self._toggles.push(toggle);
-                toggle.on('click', function() { self.changeState({tab: i+1}); });
-            });
-        }));
+    states: {
+        CREATED: {
+            setup: function() {
+                var options = this._options;
+                options.toggleSelector = options.toggleSelector || this.klass.TOGGLE_SELECTOR;
+                options.togglesClass   = options.togglesClass   || this.klass.TOGGLES_CLASS;
+                options.switchTime     = options.switchTime     || this.klass.SWITCH_TIME;
+                options.tabsPosition   = options.tabsPosition   || this.klass.INSERT_POSITION;
+                
+                this._tabGroup  = Ojay(this._tabGroup);
+                this._container = this._tabGroup.parents().at(0);
+                
+                this._addToggles();
+                
+                this._tabs = this._tabGroup.map(function(container) {
+                    return new this.klass.Tab(this, container);
+                }, this);
+                
+                if (options.width && options.height)
+                    this._container.setStyle({height: options.height});
+                
+                this.setState('READY');
+                var state = this.getInitialState();
+                this._handleSetPage(state.tab);
+                return this;
+            },
+            
+            /**
+             * <p>Insert the toggle control group before or after the tabs' containing
+             * element.</p>
+             */
+            _addToggles: function() {
+                this._toggles = [];
+                var self = this, options = self._options;
+                var toggles = Ojay( Ojay.HTML.ul({className: options.togglesClass}, function (HTML) {
+                    self._tabGroup.children(options.toggleSelector).forEach(function(header, i) {
+                        header.hide();
+                        var toggle = Ojay( HTML.li(header.node.innerHTML) ).addClass('toggle-' + i);
+                        if (i === 0) toggle.addClass('first');
+                        if (i === self._tabGroup.length - 1) toggle.addClass('last');
+                        self._toggles.push(toggle);
+                        toggle.on('click')._(self).setPage(i+1);
+                    });
+                }) );
+                
+                if (typeof this._options.width != 'undefined')
+                    toggles.setStyle({width: this._options.width});
+                
+                this._tabGroup.parents().at(0).insert(toggles, this._options.tabsPosition);
+            }
+        },
         
-        if (typeof this._options.width != 'undefined')
-            toggles.setStyle({width: this._options.width});
+        READY: {
+            /**
+             * @param {Number} page
+             * @param {Object} options
+             * @returns {Tabs}
+             */
+            setPage: function(page, options) {
+                this.changeState({tab: page});
+                if ((options || {}).silent !== false) this.notifyObservers('pagechange', page);
+                return this;
+            },
+            
+            /**
+             * <p>Switch to the tab with the index provided as the first argument.
+             * Passing in the <tt>silent</tt> option will stop the <tt>pagechange</tt>
+             * event from being published.</p>
+             *
+             * @param {Number} index
+             */
+            _handleSetPage: function(index) {
+                index  -= 1;
+                
+                if (index >= this._tabs.length) index = 0;
+                if (this._currentTab == index) return;
+                
+                if (typeof this._currentTab == 'undefined') {
+                    this._currentTab = index;
+                    this._toggles[index].addClass('current');
+                    this._tabs[index].show();
+                } else {
+                    this._toggles.forEach({removeClass: 'current'});
+                    this._toggles[index].addClass('current');
+                    this.setState('ANIMATING');
+                    this._tabs[this._currentTab].hide()._(function(self) {
+                        self._currentTab = index;
+                        self._tabs[index].show()
+                        ._(self).setState('READY');
+                    }, this);
+                }
+            }
+        },
         
-        this._tabGroup.parents().at(0).insert(toggles, this._options.tabsPosition);
-    },
-    
-    /**
-     * <p>Switch to the tab with the index provided as the first argument.
-     * Passing in the <tt>silent</tt> option will stop the <tt>tabchange</tt>
-     * event from being published.</p>
-     *
-     * @param {Number} index
-     * @param {Object} options
-     */
-    toggle: function(index, options) {
-        index  -= 1;
-        options = options || {};
-        
-        if (index >= this._tabs.length) index = 0;
-        if (this._currentTab == index || this._animating) return;
-        
-        if (typeof this._currentTab == 'undefined') {
-            this._currentTab = index;
-            this._toggles[index].addClass('current');
-            this._tabs[index].show();
-        } else {
-            this._animating = true;
-            this._toggles.forEach(function(toggle) { toggle.removeClass('current'); });
-            this._toggles[index].addClass('current');
-            this._tabs[this._currentTab].hide()._(function(self) {
-                self._currentTab = index;
-                self._tabs[index].show()._(function() {
-                    self._animating = false;
-                    if (options.silent !== true) self.notifyObservers('tabchange', index);
-                });
-            }, this);
-        }
+        ANIMATING: {}
     },
     
     extend: /** @scope Ojay.Tabs */{
@@ -144,22 +161,19 @@ Ojay.Tabs = new JS.Class(/** @scope Ojay.Tabs.prototype */{
              * @returns {JS.MethodChain}
              */
             hide: function() {
-                var chain = new JS.MethodChain;
-                this._container.animate({opacity: {to: 0}},
-                    this._group._options.switchTime).hide()
-                ._(function(self) { chain.fire(self); }, this)._(this);
-                return chain;
+                return this._container.animate({opacity: {to: 0}},
+                        this._group._options.switchTime)
+                        .hide()
+                        ._(this);
             },
             
             /**
              * @returns {JS.MethodChain}
              */
             show: function() {
-                var chain = new JS.MethodChain;
-                this._container.show().animate({opacity: {to: 1}},
-                    this._group._options.switchTime)
-                ._(function(self) { chain.fire(self); }, this)._(this);
-                return chain;
+                return this._container.show().animate({opacity: {to: 1}},
+                        this._group._options.switchTime)
+                        ._(this);
             }
         })
     }
