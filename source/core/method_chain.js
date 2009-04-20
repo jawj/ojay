ccf812f11dd19a958fcc3f926df0503f8141924c
James Coglan <jcoglan@googlemail.com>
2009-04-20 11:21:06 +0100

    Tidy up our MethodChain extensions, remove some repetition.

--- source/core/method_chain.js
+++ source/core/method_chain.js
@@ -1,20 +1,20 @@
 JS.MethodChain.addMethods(Ojay);
 
-// Modify MethodChain to allow CSS selectors
-JS.MethodChain.prototype._ = JS.MethodChain.prototype._.wrap(function() {
-    var args = Array.from(arguments), _ = args.shift();
-    if (typeof args[0] == 'string') return _(Ojay, args[0]);
-    else return _.apply(this, args);
-});
-
-(function(kernel) {
+(function() {
+    // ObjectMethods will be renamed to Kernel in JS.Class 2.1
+    var kernel = JS.ObjectMethods || JS.Kernel;
+    
+    var convertSelectors = function() {
+        var args = Array.from(arguments), _ = args.shift();
+        if (typeof args[0] == 'string') return _(Ojay, args[0]);
+        else return _.apply(this, args);
+    };
+    
+    // Modify MethodChain to allow CSS selectors
+    JS.MethodChain.prototype._ = JS.MethodChain.prototype._.wrap(convertSelectors);
+    
     kernel.include({
-        _: kernel.instanceMethod('_').wrap(function() {
-            var args = Array.from(arguments), _ = args.shift();
-            if (typeof args[0] == 'string') return _(Ojay, args[0]);
-            else return _.apply(this, args);
-        })
+        _: kernel.instanceMethod('_').wrap(convertSelectors)
     });
-// ObjectMethods will be renamed to Kernel in JS.Class 2.1
-})(JS.ObjectMethods || JS.Kernel);
+})();
 
