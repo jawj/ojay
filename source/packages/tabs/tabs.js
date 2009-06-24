aea64652349f6cbe5b56beb2a2a49193beaeea3c
jcoglan <jcoglan@googlemail.com>
2009-06-24 08:08:34 -0700

    Change the way tab toggle content is extracted from the DOM.

--- source/packages/tabs/tabs.js
+++ source/packages/tabs/tabs.js
@@ -134,7 +134,8 @@ Ojay.Tabs = new JS.Class(/** @scope Ojay.Tabs.prototype */{
                 
                 var toggles = Ojay( Ojay.HTML.ul({className: options.togglesClass}, function (HTML) {
                     self._tabGroup.children(options.toggleSelector).forEach(function(header, i) {
-                        var toggle = Ojay( HTML.li(header.node.innerHTML) ).addClass('toggle-' + (i+1));
+                        var toggle = Ojay( HTML.li() ).addClass('toggle-' + (i+1));
+                        toggle.setContent(header.node.innerHTML);
                         if (i === 0) toggle.addClass('first');
                         if (i === self._tabGroup.length - 1) toggle.addClass('last');
                         self._toggles.push(toggle);
